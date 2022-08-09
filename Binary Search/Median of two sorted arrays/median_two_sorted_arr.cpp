6e0822479951967e793147b65731f8900204ad3d
hansel-sequeira <90136917+hansel-sequeira@users.noreply.github.com>
2022-08-09 19:29:43 +0530

    Update median_two_sorted_arr.cpp

--- Binary Search/Median of two sorted arrays/median_two_sorted_arr.cpp
+++ Binary Search/Median of two sorted arrays/median_two_sorted_arr.cpp
@@ -13,16 +13,11 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
             int r1 = cut1 == n1 ? INT_MAX : nums1[cut1];
             int r2 = cut2 == n2 ? INT_MAX : nums2[cut2];
             if(l1<=r2 and l2<=r1){
-                if((n1+n2)%2 == 0){
-                    return (max(l1,l2) + min(r1,r2))/2.0;
-                } else {
-                    return max(l1,l2);
-                }
-            } else if(l1>r2){
-                high = cut1-1;
-            } else {
-                low = cut1+1;
+                if((n1+n2)%2 == 0) return (max(l1,l2) + min(r1,r2))/2.0;
+               else return max(l1,l2);
             }
+            else if(l1>r2) high = cut1-1;
+            else low = cut1+1;
         }
         return -1;
     }
