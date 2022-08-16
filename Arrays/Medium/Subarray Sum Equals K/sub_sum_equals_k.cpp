b83ee9d83dcd2a2b3f10a04ad2bce1064aaf1853
hansel-sequeira <90136917+hansel-sequeira@users.noreply.github.com>
2022-08-16 13:10:17 +0530

    Create sub_sum_equals_k.cpp

---
+++ Arrays/Medium/Subarray Sum Equals K/sub_sum_equals_k.cpp
@@ -0,0 +1,38 @@
+/*
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+*/
+
+int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size(), result = 0, sum = 0;
+        unordered_map<int,int> umap;
+        umap[0] = 1;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            if(umap.find(sum-k) != umap.end()){
+                result += umap[sum-k];
+            }
+            umap[sum]++;
+        }
+        return result;
+    }
+
