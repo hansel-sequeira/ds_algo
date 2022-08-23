4725ad1c3362d3eafaef66f069e5ec69b4ac6702
hansel-sequeira <90136917+hansel-sequeira@users.noreply.github.com>
2022-08-23 11:04:13 +0530

    Create max_prod_subarr.cpp

---
+++ Arrays/Medium/Maximum Product Subarray/max_prod_subarr.cpp
@@ -0,0 +1,44 @@
+/*
+Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+A subarray is a contiguous subsequence of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+*/
+
+int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int minval = nums[0], maxval = nums[0], res = nums[0];
+        for(int i=1;i<n;i++){
+            int oldminval = minval, oldmaxval = maxval;
+            if(nums[i]<0){
+                minval = min(oldmaxval*nums[i], nums[i]);
+                maxval = max(oldminval*nums[i], nums[i]);
+            } else {
+                minval = min(oldminval*nums[i], nums[i]);
+                maxval = max(oldmaxval*nums[i], nums[i]);
+            }
+            res = max(res, maxval);
+        }
+        return res;
+    }
