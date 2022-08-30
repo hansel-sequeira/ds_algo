16f8670b6c4872df0e79837531d6aaef68174196
hansel-sequeira <90136917+hansel-sequeira@users.noreply.github.com>
2022-08-30 13:45:24 +0530

    Create print_max_sum_subarr.cpp

---
+++ Arrays/Medium/Print maximum sum subarray/print_max_sum_subarr.cpp
@@ -0,0 +1,48 @@
+/*
+Given an array arr[], the task is to find the elements of a contiguous subarray of numbers which has the largest sum.
+
+Examples:
+
+Input: arr = [-2, -3, 4, -1, -2, 1, 5, -3]
+Output: [4, -1, -2, 1, 5]
+Explanation: 
+In the above input the maximum contiguous subarray sum is 7 and the elements of the subarray are [4, -1, -2, 1, 5]
+
+Input: arr = [-2, -5, 6, -2, -3, 1, 5, -6] 
+Output: [6, -2, -3, 1, 5] 
+Explanation: 
+In the above input the maximum contiguous subarray sum is 7 and the elements 
+of the subarray are [6, -2, -3, 1, 5]
+*/
+
+//can easily be done with O(N) space. Let's try doing it in O(1) space.
+//in normal kadane, res = max(res + nums[i], nums[i]).
+//intuitively, if nums[i] is the greatest point so far, the starting point of the printing will be that index. else it won't change.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(vector<int>& arr){
+  int n = arr.size(), sum = arr[0], res = arr[0];
+  int start_window = 0;
+  for(int i=1;i<n;i++){
+    if(arr[i] > arr[i] + sum){
+        start_window = i;
+        sum = arr[i];
+    } else {
+        sum += arr[i];
+     }
+      res = max(res, sum);
+  }
+  sum = 0;
+  while(sum!=res){
+    cout<<arr[start_window]<<" ";
+    sum += arr[start_window++];
+  }
+}
+
+int main() {
+        vector<int> arr = {-2, -5, 6, -2, -3, 1, 5, -6};
+        print(arr);
+        return 0;
+}
