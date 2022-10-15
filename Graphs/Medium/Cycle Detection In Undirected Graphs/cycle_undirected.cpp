8b52eb83d47b7fd14df133ea6dc62d357a7c034f
hansel-sequeira <90136917+hansel-sequeira@users.noreply.github.com>
2022-10-15 12:39:23 +0530

    Update cycle_undirected.cpp

--- Graphs/Medium/Cycle Detection In Undirected Graphs/cycle_undirected.cpp
+++ Graphs/Medium/Cycle Detection In Undirected Graphs/cycle_undirected.cpp
@@ -41,3 +41,40 @@ bool isCycle(int V, vector<int> adj[]) {
         }
         return false;
     }
+
+
+
+//DFS APPROACH IS THE SAME THING
+
+bool dfs(vector<int> adj[], int u, vector<bool>& vis, vector<int>& parent){
+        vis[u] = true;
+        for(int v: adj[u]){
+            if(vis[v] and parent[u]!=v)
+                return true;
+            else if(!vis[v]){
+                parent[v] = u;
+                if(dfs(adj, v, vis, parent))
+                    return true;
+            }
+        }
+        return false;
+    }
+  
+  
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        
+        //bfs
+        queue<int> q;
+        vector<bool> vis(V, false);
+        vector<int> parent(V, -1);
+        
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                if(dfs(adj, i, vis, parent))
+                    return true;
+            }
+        }
+        return false;
+    }
