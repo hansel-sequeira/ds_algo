5f30825939b128f5e1e79f568cc74c2c65e189e1
hansel-sequeira <90136917+hansel-sequeira@users.noreply.github.com>
2022-12-25 15:17:59 +0530

    Update dijkstra_algo.cpp

--- Graphs/Medium/Dijkstra's Algorithm/dijkstra_algo.cpp
+++ Graphs/Medium/Dijkstra's Algorithm/dijkstra_algo.cpp
@@ -1,3 +1,5 @@
+//tc: e*log(v)
+//Using a pq:
 public:
 	//Function to find the shortest distance of all the vertices
     //from the source vertex S.
@@ -17,6 +19,37 @@ public:
                 if(dist[v] > distance + w){
                     dist[v] = distance+w;
                     pq.push({dist[v], v});
+                }
+            }
+        }
+        return dist;
+    }
+
+
+
+//Using a set:
+
+public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Code here
+        vector<int> dist(V, INT_MAX);
+        dist[S] = 0;
+        set<pair<int,int>> s;
+        s.insert({0, S});
+        while(!s.empty()){
+            auto pair = *(s.begin());
+            s.erase(pair);
+            int distance = pair.first; int u = pair.second;
+            for(auto it: adj[u]){
+                int v = it[0]; int wt = it[1];
+                if(dist[v] > distance + wt){
+                    if(dist[v] != INT_MAX)
+                        s.erase({dist[v], v});
+                    dist[v] = distance + wt;
+                    s.insert({dist[v], v});
                 }
             }
         }
