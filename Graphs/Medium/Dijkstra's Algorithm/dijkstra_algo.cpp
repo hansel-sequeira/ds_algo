40f65f4f2d5bb88b12eb685169642af4ecc8ce3c
hansel-sequeira <90136917+hansel-sequeira@users.noreply.github.com>
2022-12-25 16:49:37 +0530

    Update dijkstra_algo.cpp

--- Graphs/Medium/Dijkstra's Algorithm/dijkstra_algo.cpp
+++ Graphs/Medium/Dijkstra's Algorithm/dijkstra_algo.cpp
@@ -1,6 +1,49 @@
-//Comparison b/w PQ and set : 
-//Incase of PQ, the maximum heap size can go upto E = number of edges, leading to complexity = O(E*logE).
-//Incase of set, the maximum set size can go upto V =. number of vertices, leading to complexity = O(E*logV).
+	// TC: ElogV
+
+//Pq implementation will take a little more space than the set. (v^2) as opposed to (v)
+//so, in case of pq, the heap will have atmost v^2 vertices. how? assume 4 nodes. A  B C D
+// A relaxes (B,C,D) and B relaxes (A,C,D) and C relaxes (A,B,D) and D relaxes (A,B,C). Then each node is being relaxes (V-1) times. ===> slight exaggeration
+//therefore, at max, the size of the heap could be v^2. In the set, it's just V
+/*
+pseudocode for pq:
+
+while(!pq.empty())                      -> V times
+{
+	dist, node = pq.top();
+	pq.pop();			-> Log(heap_size)
+	for(all nodes adjacent){        -> (V-1) at max
+		if(cond satisfied)
+			pq.push(adj node) -> Log(heap_size)
+	}
+}
+
+tc: V*(log(v^2) + (V-1)*log(v^2))   where v^2= heapsize
+=> V*log(v^2) + V^2*log(v^2) - V*log(v^2)
+=> V^2log(v^2)
+=> 2*V^2(logv)
+=> v^2 * log(v)
+=> 2E * log (v) => e*log(v) since 2E = V^2
+
+Similarly, for a set:
+
+while(!s.empty())                      -> V times
+{
+	dist, node = s.begin();         
+	s.erase();			-> Log(setSize)
+	for(all nodes adjacent){        -> (V-1) at max
+		if(cond satisfied)
+			s.insert(adj node) -> Log(setSize)
+	}
+}
+V*(log(setSize) + (V-1)*(log(setSize))
+=> V*log(ss) + V^2(log(ss)) - V*(log(ss))
+=> V^2(logss)
+=> V^2*log(V)
+=> 2*E*log(V)
+=> E*log(V)
+*/
+
+
 //Using a pq:
 public:
 	//Function to find the shortest distance of all the vertices
